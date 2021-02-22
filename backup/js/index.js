4bd11bed19e617fb333e38da718f34a1830dae0a
Anton Fomin <anton.fomi@ya.ru>
2021-02-22 21:48:15 +0300

    updated

--- backup/js/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = async function () {
-    await render('intro');
-};
-
-async function render(data){
-    let response = await fetch(`/html/${data}.html`);
-    data = await response.text();
-    document.getElementById("main").innerHTML = data;
-}
