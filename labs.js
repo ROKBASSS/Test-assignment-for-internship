85f070b98d68d615e270172b272fa35a43229129
Anton Fomin <anton.fomi@ya.ru>
2021-02-22 22:44:19 +0300

    updated

---
+++ labs.js
@@ -0,0 +1,30 @@
+function init() {
+    goss11.addEventListener("click", function () {
+        window.location.href="./labs/xml/lab11/index.xml"
+    });
+    goss12.addEventListener("click", function () {
+        window.location.href="./labs/xml/lab12/index.xml"
+    });
+    goss21.addEventListener("click", function () {
+        window.location.href="./labs.html"
+    });
+    goss22.addEventListener("click", function () {
+        window.location.href="./labs.html"
+    });
+}
+
+
+function doSomethingElse() {
+    fetch('./labs/xml/lab1/index.xml').then(response => {
+        return response.text();
+    }).then(data => {
+        let parser = new DOMParser(),
+            xmlDoc = parser.parseFromString(data, 'text/xml');
+        console.log(xmlDoc)
+    })
+}
+
+window.onload = function () {
+    init();
+    doSomethingElse();
+};
